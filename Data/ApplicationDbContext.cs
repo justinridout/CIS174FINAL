f9543212df1f3afa85e5c53fc2b34e5f7e4c415c
justinridout <56090823+justinridout@users.noreply.github.com>
2019-12-04 14:22:33 -0600

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Goals> Goals { get; set; }
+        public DbSet<WorkoutDetailed> WorkoutDetailed { get; set; }
+        public DbSet<WorkoutSummary> WorkoutSummary { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Server=tcp:finalprojectcis174.database.windows.net,1433;Initial Catalog=FinalProjectCIS174;Persist Security Info=False;User ID=finalproject;Password=(Cis174);MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+}
+}
